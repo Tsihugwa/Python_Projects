25b85bb0cb125ef69b9061f113f9a53dd6d8d5c7
Tsihugwa <101879237+Tsihugwa@users.noreply.github.com>
2023-01-08 03:52:44 +0000

    Update Email _slicer.py

--- Email _slicer.py
+++ Email _slicer.py
@@ -1,19 +1,24 @@
+# First, we'll import the necessary libraries
+import os
 import re
 
-def slice_email(email):
-  # Use a regular expression to extract the username and domain from the email
-  username, domain = re.match(r"([^@]+)@([^@]+)", email).groups()
-  return username, domain
+# Next, we'll prompt the user for their email address
+email = input("Enter your email address: ")
 
-def customize_and_send_message(email, message):
-  # Slice the email address
-  username, domain = slice_email(email)
-  
-  # Customize the message
-  customized_message = f"Dear {username},\n\n{message}\n\nBest regards,\nYour friendly Email Slicer"
-  
-  # Send the message
-  send_email(email, customized_message)
+# We'll use a regular expression to validate the email address
+pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+if not re.match(pattern, email):
+  print("Invalid email address. Please try again.")
+  exit()
 
-# Example usage
-customize_and_send_message("user@example.com", "Thank you for using our Email Slicer service!")
+# If the email address is valid, we'll slice it to get the username and domain
+username = email[:email.index("@")]
+domain = email[email.index("@")+1:]
+
+# Now we can customize and send a message to the user with this information
+message = f"Your email address is: {email}\nYour username is: {username}\nYour domain is: {domain}"
+print(message)
+
+# Finally, we can use the `os` library to send an email to the user
+# You'll need to customize the `sendmail` command with the appropriate email addresses and message
+os.system(f"echo {message} | mail -s 'Email Slicer Results' {email}")
